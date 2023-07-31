e04cb45a8ab92a6629f5b5e1ae7b7d0d01206057
Aram Hammoudeh <aram.hammoudeh@indr.com>
2023-07-31 00:43:12 -0600

    patch type generator

--- libs/shared/types/src/generated/trackable.ts
+++ libs/shared/types/src/generated/trackable.ts
@@ -1,10 +1,31 @@
 
 
+/**
+ * Trackable
+ */
 export interface Trackable {
+  /**
+   * archivedAt
+   */
   archivedAt: Date;
+  /**
+   * archivedBy
+   */
   archivedBy: string;
+  /**
+   * createdAt
+   */
   createdAt: Date;
+  /**
+   * createdBy
+   */
   createdBy: string;
+  /**
+   * updatedAt
+   */
   updatedAt: Date;
+  /**
+   * updatedBy
+   */
   updatedBy: string;
 }
--- tools/type-generator/index.ts
+++ tools/type-generator/index.ts
@@ -1,5 +1,6 @@
 import { convertFromDirectory } from 'joi-to-typescript';
 import { settings } from './constants';
+import fs from 'fs';
 
 async function types(): Promise<void> {
   // eslint-disable-next-line no-console
@@ -13,6 +14,21 @@ async function types(): Promise<void> {
   if (result) {
     // eslint-disable-next-line no-console
     console.log('Completed joi-to-typescript');
+
+    // some of the files in settings.typeOutputDirectory are having the text `import { undefined } from '.';` added to the top of the file. use fs to remove this line from any files in the typeOutputDirectory
+    const files = fs.readdirSync(settings.typeOutputDirectory);
+    files.forEach((file) => {
+      const filePath = `${settings.typeOutputDirectory}/${file}`;
+      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const lines = fileContents.split('\n');
+      const filteredLines = lines.filter(
+        (line) => !line.includes("import { undefined } from '.';")
+      );
+      const newFileContents = filteredLines.join('\n');
+
+      // write the new file contents to the file
+      fs.writeFileSync(filePath, newFileContents);
+    });
   } else {
     // eslint-disable-next-line no-console
     console.log('Failed to run joi-to-typescrip');
