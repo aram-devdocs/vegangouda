bc1f5230e06200ac06fe4e2f51a767245208c75e
Aram Hammoudeh <aram.devdocs@gmail.com>
2024-08-12 14:45:32 -0600

    refactor: Update database connection and error handling

--- apps/api/src/app/plugins/db.ts
+++ apps/api/src/app/plugins/db.ts
@@ -3,25 +3,40 @@ import { Pool } from 'pg';
 import { createTables } from '../utils/seed';
 
 const DATABASE_URL = process.env.DATABASE_URL;
+console.log(DATABASE_URL);
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
 });
-export const db = {
+
+let db = {
   query: (text: string, params: any) => pool.query(text, params),
   getClient: () => pool.connect(),
 };
 
 // test connection
 
-db.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('connected to postgres db');
-  }
-});
+try {
+  db.query('SELECT NOW()', (err, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('connected to postgres db');
+    }
+  });
+  createTables().then((r) => {
+    console.log(r);
+  });
+} catch (error) {
+  console.log(error);
+  db = {
+    query: () => {
+      console.error('db not connected');
+    },
+    getClient: () => {
+      console.error('db not connected');
+    },
+  };
+}
 
-createTables().then((r) => {
-  console.log(r);
-});
+export { db };
