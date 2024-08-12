e5ad930b9c23556ae89fe981fb49741cb9c34e92
Aram Hammoudeh <aram.devdocs@gmail.com>
2024-08-12 17:10:16 -0600

    refactor: Update database connection and error handling

--- apps/api/src/app/plugins/db.ts
+++ apps/api/src/app/plugins/db.ts
@@ -7,36 +7,25 @@ console.log(DATABASE_URL);
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
+}).on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
 });
 
-let db = {
+export const db = {
   query: (text: string, params: any) => pool.query(text, params),
   getClient: () => pool.connect(),
 };
 
 // test connection
 
-try {
-  db.query('SELECT NOW()', (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('connected to postgres db');
-    }
-  });
-  createTables().then((r) => {
-    console.log(r);
-  });
-} catch (error) {
-  console.log(error);
-  db = {
-    query: () => {
-      console.error('db not connected');
-    },
-    getClient: () => {
-      console.error('db not connected');
-    },
-  };
-}
-
-export { db };
+// db.query('SELECT NOW()', (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     throw err;
+//   } else {
+//     console.log('connected to postgres db');
+//   }
+// });
+// createTables().then((r) => {
+//   console.log(r);
+// });
