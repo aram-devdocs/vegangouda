2d9edefc46fffdb10271c8276e1bd4ec518283b0
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-08-16 16:05:29 -0600

    chore: Update baseURL to use correct API URL

--- libs/web/design-system/src/lib/constants.ts
+++ libs/web/design-system/src/lib/constants.ts
@@ -74,7 +74,7 @@ export const api_port = process.env.API_PORT || 3000;
 export const baseURL =
   process.env.REACT_APP_API_URL ||
   process.env.NODE_ENV === 'production'
-    ? 'http://10.0.0.42:3000' // TODO: Get the correct URL from API_URL
+    ? 'http://10.0.0.40:3000' // TODO: Get the correct URL from API_URL
     : `http://localhost:${api_port}`;
 export const defaultTimeout = 1000;
 
